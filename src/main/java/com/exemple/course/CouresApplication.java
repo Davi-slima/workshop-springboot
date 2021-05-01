264e987e251c839fb5486948d11a1284a328f008
Davi dos Santos <davi.slima75@gmail.com>
2021-04-30 22:00:49 -0300

    Project created

---
+++ src/main/java/com/exemple/course/CouresApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouresApplication.class, args);
+	}
+
+}
